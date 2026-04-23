4bc4cf78d199cb4e525c68fac1d27b3de8c73598
CrisARamirez <crisr4mirezzg@gmail.com>
2026-04-23 10:27:35 -0500

    Config linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,9 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
+import importPlugin from 'eslint-plugin-import';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
 export default tseslint.config(
   {
     ignores: ['eslint.config.mjs'],
@@ -12,6 +15,10 @@ export default tseslint.config(
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
   {
+    plugins: {
+      import: importPlugin,
+      'simple-import-sort': simpleImportSort,
+    },
     languageOptions: {
       globals: {
         ...globals.node,
@@ -23,13 +30,24 @@ export default tseslint.config(
         tsconfigRootDir: import.meta.dirname,
       },
     },
+    settings: {
+      'import/resolver': {
+        typescript: true,
+      },
+    },
   },
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
+
       "prettier/prettier": ["error", { endOfLine: "auto" }],
+
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+
+      'import/order': 'off',
     },
   },
 );
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LeadsModule } from './leads/leads.module';
 
 @Module({
-  imports: [],
+  imports: [LeadsModule],
   controllers: [AppController],
   providers: [AppService],
 })
